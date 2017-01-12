9aff14f8a15fa91f09edf2932422b974ec474957
kotleta <jevgenij.kariagin@gmail.com>
2017-01-12 19:07:36 +0200

    Change1

--- Program.java
+++ Program.java
@@ -1,7 +1,14 @@
 public class Program
+
 {
- public static void main(String[]args)
- {
-  System.out.println("Hello Project3");
- }
+ 
+public static void main(String[]args)
+ 
+{
+  
+System.out.println("Hello Project3");
+ 
+System.out.println("Project3 change1");
+}
+
 }
